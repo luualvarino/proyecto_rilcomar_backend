c8abb37ba657f9fc22c51511a75d0f3ecb7aa07e
luualvarino <lulualvarino@gmail.com>
2024-10-26 15:04:34 -0300

    Start
    Agregue proyecto starter backend

---
+++ rilcomar/src/main/java/com/proyecto/rilcomar/RilcomarApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.rilcomar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RilcomarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RilcomarApplication.class, args);
+	}
+
+}
