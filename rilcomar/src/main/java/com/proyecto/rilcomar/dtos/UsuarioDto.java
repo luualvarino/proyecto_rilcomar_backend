a9cbf775f86678a85add1aa9da7217799ec026a2
Sancho Martinez <sanchom99@gmail.com>
2025-01-29 01:04:57 -0300

    Modifique autenticarUsuario Modifique agregarUsuario Modifique usuarioDto Agregue build al clienteDto Todo lo hice para que devuelva el cliente y agregue al cliente al usuario.

--- rilcomar/src/main/java/com/proyecto/rilcomar/dtos/UsuarioDto.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/dtos/UsuarioDto.java
@@ -12,12 +12,22 @@ public class UsuarioDto {
     private String username;
     private String password;
     private boolean esAdmin;
+    private ClienteDto cliente;
+
+
+
 
     public static UsuarioDto build(Usuario usuario){
         return UsuarioDto.builder()
                 .username(usuario.getUsername())
                 .password(usuario.getPassword())
                 .esAdmin(usuario.isEsAdmin())
+                .cliente(usuario.getCliente() != null ? ClienteDto.builder()
+                        .id(usuario.getCliente().getId())
+                        .nombre(usuario.getCliente().getNombre())
+                        .telefono(usuario.getCliente().getTelefono())
+                        .mail(usuario.getCliente().getMail())
+                        .build() : null)
                 .build();
     }
 }
--- rilcomar/src/main/java/com/proyecto/rilcomar/services/UsuarioService.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/services/UsuarioService.java
@@ -2,6 +2,7 @@ package com.proyecto.rilcomar.services;
 import java.util.List;
 import com.proyecto.rilcomar.entities.Usuario;
 import com.proyecto.rilcomar.repos.UsuarioRepository;
+import com.proyecto.rilcomar.repos.ClienteRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,23 +12,33 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-    public Usuario autenticarUsuario(Usuario c) {
-        Usuario usuario = usuarioRepository.findById(c.getUsername()).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+    public Usuario autenticarUsuario(Usuario usuario) {
+        Usuario usuarioEncontrado = usuarioRepository.findById(usuario.getUsername())
+                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
 
-        if (usuario.getPassword().equals(c.getPassword())) {
-            return usuario;
+        if (usuarioEncontrado.getPassword().equals(usuario.getPassword())) {
+            return usuarioEncontrado;
         } else {
             throw new RuntimeException("Contraseña incorrecta");
         }
     }
 
 
-    public Usuario agregarUsuario(Usuario usuario) {
-        try{
-            if (usuarioRepository.existsById(usuario.getUsername()))
+    public Usuario agregarUsuario(Usuario usuario, Integer idCliente) {
+        try {
+            if (usuarioRepository.existsById(usuario.getUsername())) {
                 throw new RuntimeException("El nombre de usuario ya existe");
-            else return usuarioRepository.save(usuario);
+            }
+
+            if (!usuario.isEsAdmin() && idCliente != null) {
+                usuario.setCliente(clienteRepository.findById(idCliente)
+                        .orElseThrow(() -> new RuntimeException("Cliente no encontrado")));
+            }
+
+            return usuarioRepository.save(usuario);//no me interesa que al agregar el usuario devuelvatodo
         } catch (Exception e) {
             throw new RuntimeException("Error inesperado al guardar el usuario", e);
         }
