7562c16b6a1b9d435cb2ce967ac090cbfe13cbf7
lucia.alvarino <lucia.alvarino@qoxit.com>
2025-02-08 21:54:30 -0300

    Agregue CORS para deploy frontend

--- rilcomar/src/main/java/com/proyecto/rilcomar/configuration/WebConfiguration.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/configuration/WebConfiguration.java
@@ -4,12 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(String.valueOf(List.of("https://red-hill-05804ba0f.4.azurestaticapps.net")))
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
