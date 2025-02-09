4a3be5b749cc25199e5ce72bec6b1c5ce40a6095
lucia.alvarino <lucia.alvarino@qoxit.com>
2025-02-09 15:01:40 -0300

    Config

--- rilcomar/src/main/java/com/proyecto/rilcomar/configuration/WebConfiguration.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/configuration/WebConfiguration.java
@@ -2,18 +2,18 @@ package com.proyecto.rilcomar.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
+@EnableWebMvc
 public class WebConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins(String.valueOf(List.of("https://red-hill-05804ba0f.4.azurestaticapps.net")))
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins("https://red-hill-05804ba0f.4.azurestaticapps.net")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
