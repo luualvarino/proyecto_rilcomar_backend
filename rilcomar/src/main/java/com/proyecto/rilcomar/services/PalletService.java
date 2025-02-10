7e9b11e8ec0ba8fd1e623d6287a966580d56fbf4
giuubellomo <giulianabellomo02@gmail.com>
2025-02-10 20:54:48 -0300

    url

--- rilcomar/src/main/java/com/proyecto/rilcomar/services/PalletService.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/services/PalletService.java
@@ -52,7 +52,7 @@ public class PalletService {
             nuevoPallet = palletRepository.save(nuevoPallet);
 
             try {
-                String palletUrl = "http://rilcomarv2-a6gjaxgyfsgdebey.eastus2-01.azurewebsites.net/rilcomar/pallets/" + nuevoPallet.getId();
+                String palletUrl = "https://red-hill-05804ba0f.4.azurestaticapps.net/rilcomar/pallets/" + nuevoPallet.getId();
 
                 nuevoPallet.setQrCodeUrl(palletUrl);
 
