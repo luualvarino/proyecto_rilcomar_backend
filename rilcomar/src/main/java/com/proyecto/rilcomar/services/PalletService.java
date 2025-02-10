551f4ee46f32ec6d612a42764bf1ce7c9beb9f06
giuubellomo <giulianabellomo02@gmail.com>
2025-02-10 20:40:28 -0300

    cambio de url y configuracion con frontend

--- rilcomar/src/main/java/com/proyecto/rilcomar/services/PalletService.java
+++ rilcomar/src/main/java/com/proyecto/rilcomar/services/PalletService.java
@@ -20,8 +20,6 @@ public class PalletService {
     @Autowired
     QRCodeGeneratorService qrCodeGeneratorService;
 
-    @Value("qr.url")
-    private String qrUrl;
 
     public List<Pallet> obtenerPallets(String estado, String tipo, String formato) {
         MaterialEnum materialEnum = tipo != null ? MaterialEnum.valueOf(tipo) : null;
@@ -52,7 +50,7 @@ public class PalletService {
             nuevoPallet = palletRepository.save(nuevoPallet);
 
             try {
-                String palletUrl = "http://rilcomarv2-a6gjaxgyfsgdebey.eastus2-01.azurewebsites.net/rilcomar/pallets/" + nuevoPallet.getId();
+                String palletUrl = "https://red-hill-05804ba0f.4.azurestaticapps.net/rilcomar/pallets/" + nuevoPallet.getId();
 
                 nuevoPallet.setQrCodeUrl(palletUrl);
 
